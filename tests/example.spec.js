96d50efa6abdc43fbae0ef51a020074c091da9d6
Sharath LS <sharath.ls@feuji.com>
2024-02-28 15:14:16 +0530

    Accessibility Testing added

---
+++ tests/example.spec.js
@@ -0,0 +1,27 @@
+const { test, expect } = require('@playwright/test');
+const { default: AxeBuilder } = require('@axe-core/playwright');
+import { createHtmlReport } from 'axe-html-reporter';
+const fs = require('fs');
+const { exec } = require('child_process'); 
+test.describe('homepage', () => {
+  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+ 
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+ 
+   
+    const reportHTML = createHtmlReport({
+      results: accessibilityScanResults,
+      options: {
+        projectKey: "homepage"
+      },
+    });
+    if (!fs.existsSync("build/reports/accessibility-report.html")) {
+      fs.mkdirSync("build/reports", {
+        recursive: true,
+      });
+    }
+    fs.writeFileSync("build/reports/accessibility-report.html", reportHTML);
+    exec('start build/reports/accessibility-report.html');
+  })
+});
